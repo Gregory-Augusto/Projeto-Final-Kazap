311d01a99289463f8bae4e8bfb09fa40e5dbe65b
Rogerio Augusto <raa.augusto@hotmail.com>
2023-08-03 22:53:42 -0300

    Arrumando o app.rb

--- Projeto Final/app.rb
+++ Projeto Final/app.rb
@@ -1,24 +1,22 @@
 require 'sequel'
+
 DB = Sequel.sqlite('db/bank.db')
 
-
-require_relative 'models/client'
-require_relative 'models/phone'
-require_relative 'models/account'
-require_relative 'models/transfer'
-require_relative 'models/pix'
-require_relative 'models/ted'
-require_relative 'models/transaction'
-require_relative 'models/deposit'
-require_relative 'models/withdraw'
-require_relative 'models/overdraft'
-
+require './models/client'
+require './models/phone'
+require './models/account'
+require './models/transfer'
+require './models/pix'
+require './models/ted'
+require './models/transaction'
+require './models/deposit'
+require './models/withdraw'
+require './models/overdraft'
 
 require './modules/module_deposit'
 require './modules/module_pix'
 require './modules/module_ted'
 require './modules/module_withdraw'
-
 
 require './classes/class_account'
 require './classes/class_client'
